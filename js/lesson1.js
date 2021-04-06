d5e8d0d7fa9758bcdf3fbeea84b09690f71ab08e
Tatyana Egorenkova <tatyana.baeva.1990@gmail.com>
2021-04-06 22:03:32 +0700

    переменные и константы

--- js/lesson1.js
+++ js/lesson1.js
@@ -1,2 +1,13 @@
-let obj = document.getElementById('id_div');
-console.log(obj);
+/* это моя первая программа  */
+
+'use strict';
+
+let user = 'Tanya',
+    age = 22,
+    email = 'tanya@mail.ru';
+
+age = 33
+
+const PI = 3.1415;
+
+console.log(user, age, email, PI);
